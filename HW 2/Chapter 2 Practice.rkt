7a6379831f49017168238e6c501c3cbeee95ed69
sbernheim4 <sambernheim@gmail.com>
2016-02-05 02:18:03 -0500

    added comments

--- HW 2/Chapter 2 Practice.rkt
+++ HW 2/Chapter 2 Practice.rkt
@@ -1,14 +1,24 @@
+; two ways to define the make-rat fucntion
+; this way does not reduce the value to lowest term upon creation
 (define (make-rat n d) (cons n d))
+; this does create a reduced number when the number is created
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
 
+; get the numerator of the number
 (define (numer x) (car x))
+; get the denominator of the number 
 (define (denom x) (cdr x))
 
+; print the number 
 (define (print-rat x)
   (newline)
   (display (numer x))
   (display "/")
   (display (denom x)))
 
+; add, subtract, multiply, and divide the number 
 (define (add-rat x y)
   (make-rat (+ (* (numer x) (denom y))
                (* (numer y) (denom x)))
@@ -23,15 +33,10 @@
 (define (div-rat x y)
   (make-rat (* (numer x) (denom y))
             (* (denom x) (numer y))))
+
+; test if two numbers are equal 
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
-
-(define (make-rat n d)
-  (let ((g (gcd n d)))
-    (cons (/ n g) (/ d g))))
-
-
-
 
 
