39bd2afa4777e1e56bdb960eeca0dcd20979056e
sbernheim4 <sambernheim@gmail.com>
2016-02-05 15:55:51 -0500

    beginning work on exercise 2.27

---
+++ HW 2/ex2.18.rkt
@@ -0,0 +1,29 @@
+; This procedure gets the last element in a list by recursively calling cdr on a list until the list is empty
+; The last element in the list is returned. 
+(define (get-last param)
+  (define (get-last-iter param last)
+  (if (null? param)
+      last
+      (get-last-iter (cdr param) (car param))))
+  (get-last-iter param 0))
+
+; the size of the list is found by adding 1 to a counter variable every time the the size-iter loop is run
+(define (size l)
+  (define (size-iter l count)
+    (if (null? l)
+        count
+        (size-iter (cdr l) (+ 1 count))))
+  (size-iter l 0)
+)
+
+; the list in reverse is returned 
+(define (reverse x)
+    (define (reverse-iter orig new)
+      (let ((l (get-last orig)))
+      (if (= (size x) (size new))
+          new
+          (reverse-iter (cdr orig) (append (list (car orig)) new))))
+      )
+  (reverse-iter x (list ))
+)
+
