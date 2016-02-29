02487f5bef9f9420531bb8ea3f0b4f022db20c85
sbernheim4 <sambernheim@gmail.com>
2016-02-28 23:14:23 -0500

    updated writeup

--- HW 2/ex2.28.scm
+++ HW 2/ex2.28.scm
@@ -1,17 +1,8 @@
 (define x (list (list 1 2) (list 3 4)))
 
 (define (fringe tree lst)
-  (let ((last-element (get-last tree)))
-    (append lst last-element)
-    (fringe (car tree) lst)
-  ))
-
-(define (get-last param)
-  (define (get-last-iter param last)
-    (if (null? param)
-        last
-        (get-last-iter (cdr param) (car param))))
-  (get-last-iter param 0))
+    (define new-lst (append lst (car tree)))
+    (fringe (cdr tree) new-lst))
 
 
 (define (size l)
