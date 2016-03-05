4944db0e73afd1e0917b4273debaf926d594f85e
sbernheim4 <sambernheim@gmail.com>
2016-03-05 16:09:57 -0500

    began HW 3

---
+++ HW 3/Chapter 3 Practice.rkt
@@ -0,0 +1,33 @@
+(define balance 100)
+
+(define (withdraw amt)
+  (if (>= balance amt)
+      (begin (set! balance (- balance amt))
+             balance)
+      "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+(define (make-account balance)
+  (define (withdraw amt)
+    (if (>= balance amt)
+        (begin (set! balance (- balance amt))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amt)
+    (if (> amt 0)
+        (begin (set! balance (+ balance amt))
+               balance)
+        (withdraw (abs amt))))
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+m))))
+dispatch)
